0fbd403005e809aa3a5f0a871065920efe1c2d88
André <aldeoliveira03@gmail.com>
2023-08-25 12:08:02 -0300

    feat(auth): add devise authentication for Admin and Profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :profiles
+  devise_for :admins
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root 'welcome#index'
