8b4fd35249f144165551b640e0b6540651dc7b12
André <aldeoliveira03@gmail.com>
2023-08-25 13:24:45 -0300

    feat(layouts): create parent layouts for site, admins and profiles backoffices

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :site do
+    get 'welcome/index'
+  end
   namespace :profiles_backoffice do
     get 'welcome/index'
   end
@@ -9,5 +12,5 @@ Rails.application.routes.draw do
   devise_for :admins
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  root 'welcome#index'
+  root 'site/welcome#index'
 end
