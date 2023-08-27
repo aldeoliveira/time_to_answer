869a903ccdcee063012d73b7bcd36c33edcdb2a8
André <aldeoliveira03@gmail.com>
2023-08-26 23:39:02 -0300

    feat(assets): add bootstrap

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,6 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+
+//= require jquery/dist/jquery.js
+//= require bootstrap/dist/js/bootstrap.js
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 pin "jquery", to: "jquery/dist/jquery.js"
+pin "bootstrap", to: "bootstrap/dist/js/bootstrap.js"
