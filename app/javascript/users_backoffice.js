839f8a69e5f04d4cee01f4dca6c30b4b483699fc
André <aldeoliveira03@gmail.com>
2023-08-28 16:12:33 -0300

    feat(templates): add assets to asset pipeline

--- app/javascript/users_backoffice.js
+++ app/javascript/users_backoffice.js
@@ -1,1 +1,8 @@
+//= require jquery/dist/jquery
+//= require bootstrap/dist/js/bootstrap
+//= require fastclick/lib/fastclick
+//= require nprogress/nprogress
+//= require malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar
+//= require gentelella-custom
+
 console.log("users_backoffice.js");
