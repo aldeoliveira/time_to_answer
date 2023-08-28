eba69bbca3e96d744a846b742396c792a3a0b475
André <aldeoliveira03@gmail.com>
2023-08-28 17:59:31 -0300

    fix(assets): adjust assets' address

--- app/javascript/users_backoffice.js
+++ app/javascript/users_backoffice.js
@@ -1,8 +1,6 @@
-//= require jquery/dist/jquery
+//= require jquery-2.2.3/dist/jquery
 //= require bootstrap/dist/js/bootstrap
 //= require fastclick/lib/fastclick
 //= require nprogress/nprogress
 //= require malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar
-//= require gentelella-custom
-
-console.log("users_backoffice.js");
+//= require custom
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,8 +11,24 @@ Rails.application.config.assets.paths << Rails.root.join("vendor", "stylesheets"
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( jquery.js )
-Rails.application.config.assets.precompile += %w( bootstrap/dist/js/bootstrap.js bootstrap.css )
-Rails.application.config.assets.precompile += %w( metismenu/dist/metisMenu.js metisMenu.css )
+
+# /app/assets
+Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_backoffice.css )
+Rails.application.config.assets.precompile += %w( users_backoffice.js users_backoffice.css )
+
+# /lib/assets
+Rails.application.config.assets.precompile += %w( img.jpg )
+
+# /vendor
+Rails.application.config.assets.precompile += %w( jquery-2.2.3/dist/jquery.js )
+Rails.application.config.assets.precompile += %w( custom.js custom.css )
 Rails.application.config.assets.precompile += %w( sb-admin-2.js sb-admin-2.css )
-Rails.application.config.assets.precompile += %w( font-awesome.css )
+
+# /node_modules
+Rails.application.config.assets.precompile += %w( bootstrap/dist/js/bootstrap.js bootstrap/dist/css/bootstrap.css )
+Rails.application.config.assets.precompile += %w( fastclick/lib/fastclick.js )
+Rails.application.config.assets.precompile += %w( font-awesome/css/font-awesome.css )
+Rails.application.config.assets.precompile += %w( jquery/dist/jquery.js )
+Rails.application.config.assets.precompile += %w( malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.js malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css )
+Rails.application.config.assets.precompile += %w( metismenu/dist/metisMenu.js metismenu/dist/metisMenu.css )
+Rails.application.config.assets.precompile += %w( nprogress/nprogress.js nprogress/nprogress.css )
