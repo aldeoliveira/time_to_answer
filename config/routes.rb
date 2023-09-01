d647f52e50c72e4444a74014dd7d448ee0466b8f
André <aldeoliveira03@gmail.com>
2023-09-01 15:43:33 -0300

    feat(admins): create admin

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   namespace :admins_backoffice do
     get 'welcome/index'
-    resources :admins, only: [:index, :edit, :update]
+    resources :admins, except: [:destroy]
   end
 
   namespace :users_backoffice do
