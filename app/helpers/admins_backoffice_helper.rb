06e6252277ea905c76cf1a038ecc99832bced563
André <aldeoliveira03@gmail.com>
2023-09-05 02:12:23 -0300

    refact(admins): use i18n to reference model attributes in views

--- app/helpers/admins_backoffice_helper.rb
+++ app/helpers/admins_backoffice_helper.rb
@@ -1,2 +1,9 @@
 module AdminsBackofficeHelper
+  def translate_attribute(object=nil, method=nil)
+    if not (object or method)
+      "Informe os parâmetros corretamente"
+    end
+
+    object.model.human_attribute_name(method)
+  end
 end
