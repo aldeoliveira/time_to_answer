806e56de6faa0b9ccc6041a75ac0c701ab500c22
André <aldeoliveira03@gmail.com>
2023-09-10 14:56:03 -0300

    refact(users): add new templates for user_devise and login

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,22 +13,20 @@ Rails.application.config.assets.paths << Rails.root.join("vendor", "stylesheets"
 # folder are already added.
 
 # /app/assets
-Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_backoffice.css )
-Rails.application.config.assets.precompile += %w( users_backoffice.js users_backoffice.css )
+Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_backoffice.css
+                                                  users_backoffice.js users_backoffice.css
+                                                  admin_devise.js admin_devise.css
+                                                  user_devise.js user_devise.css )
 
 # /lib/assets
 Rails.application.config.assets.precompile += %w( img.jpg )
 
 # /vendor
-Rails.application.config.assets.precompile += %w( jquery-2.2.3/dist/jquery.js )
-Rails.application.config.assets.precompile += %w( custom.js custom.css )
-Rails.application.config.assets.precompile += %w( sb-admin-2.js sb-admin-2.css )
+Rails.application.config.assets.precompile += %w( jquery-2.2.3/dist/jquery.js
+                                                  custom.js custom.css
+                                                  sb-admin-2.js sb-admin-2.css )
 
 # /node_modules
-Rails.application.config.assets.precompile += %w( bootstrap/dist/js/bootstrap.js bootstrap/dist/css/bootstrap.css )
-Rails.application.config.assets.precompile += %w( fastclick/lib/fastclick.js )
-Rails.application.config.assets.precompile += %w( font-awesome/css/font-awesome.css )
-Rails.application.config.assets.precompile += %w( jquery/dist/jquery.js )
-Rails.application.config.assets.precompile += %w( malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.js malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css )
-Rails.application.config.assets.precompile += %w( metismenu/dist/metisMenu.js metismenu/dist/metisMenu.css )
-Rails.application.config.assets.precompile += %w( nprogress/nprogress.js nprogress/nprogress.css )
+Rails.application.config.assets.precompile += %w( jquery/dist/jquery.js
+                                                  bootstrap/dist/js/bootstrap.js
+                                                  metismenu/dist/metisMenu.js )
