fb14e14920be8f7f780759ea626f0ee6a7e4f8b3
André <aldeoliveira03@gmail.com>
2023-09-15 13:43:52 -0300

    refact(site): verify answers with javascript

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
+// import "@hotwired/turbo-rails"
 import "controllers"
+import Rails from "@rails/ujs"
 
-// Turbo.setFormMode("off");
+Rails.start();
