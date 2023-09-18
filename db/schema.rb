c73b67d9d4a8417baa5d414260f280c2c07da127
André <aldeoliveira03@gmail.com>
2023-09-18 11:37:12 -0300

    feat(site): add questions count to subjects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_04_023839) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_18_142312) do
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -44,6 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_04_023839) do
     t.string "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "questions_count"
   end
 
   create_table "users", force: :cascade do |t|
