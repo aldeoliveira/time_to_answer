2004d45871dff213c83313a4ccec8c5a15fd8098
André <aldeoliveira03@gmail.com>
2023-10-02 11:46:47 -0300

    feat(users): user name validation

--- app/helpers/users_backoffice_helper.rb
+++ app/helpers/users_backoffice_helper.rb
@@ -1,2 +1,9 @@
 module UsersBackofficeHelper
+  def user_name
+    if (current_user.first_name||current_user.last_name)
+      current_user.full_name
+    else
+      current_user.email
+    end
+  end
 end
