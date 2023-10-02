bcb18f3214e8dfdde5d73531c97499995d939017
André <aldeoliveira03@gmail.com>
2023-10-02 11:13:26 -0300

    feat(users): edit user profile

--- app/javascript/users_backoffice.js
+++ app/javascript/users_backoffice.js
@@ -4,4 +4,5 @@
 //= require fastclick/lib/fastclick
 //= require nprogress/nprogress
 //= require malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar
+//= require bootstrap-growl-ifightcrime/jquery.bootstrap-growl
 //= require custom
