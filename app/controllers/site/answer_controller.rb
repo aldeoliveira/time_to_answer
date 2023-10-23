8e498cf43135822f136ae8a9926768068b43c974
André <aldeoliveira03@gmail.com>
2023-10-23 15:34:35 -0300

    feat(users): register quantity of right and wrong answers by the user

--- app/controllers/site/answer_controller.rb
+++ app/controllers/site/answer_controller.rb
@@ -1,5 +1,16 @@
 class Site::AnswerController < SiteController
   def evaluate
     @answer = Answer.find(params[:answer_id])
+    set_user_statistics(@answer)   
   end
+
+  private
+
+    def set_user_statistics(answer)
+      if user_signed_in?
+        user_statistics = UserStatistics.find_or_create_by!(user: current_user)
+        (answer.correct?) ? (user_statistics.right_answers += 1) : (user_statistics.wrong_answers += 1)
+        user_statistics.save
+      end
+    end
 end
