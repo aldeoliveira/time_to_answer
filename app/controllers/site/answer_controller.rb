3c1fdea3f173cb5f1d815bd589944e5de82a2d4e
André <aldeoliveira03@gmail.com>
2023-10-23 20:35:48 -0300

    refact(users): move set_statistics method to model

--- app/controllers/site/answer_controller.rb
+++ app/controllers/site/answer_controller.rb
@@ -1,16 +1,6 @@
 class Site::AnswerController < SiteController
   def evaluate
     @answer = Answer.find(params[:answer_id])
-    set_user_statistics(@answer)   
-  end
-
-  private
-
-    def set_user_statistics(answer)
-      if user_signed_in?
-        user_statistics = UserStatistics.find_or_create_by!(user: current_user)
-        (answer.correct?) ? (user_statistics.right_answers += 1) : (user_statistics.wrong_answers += 1)
-        user_statistics.save
-      end
-    end
+    UserStatistics.set_statistics(@answer, current_user) 
+  end   
 end
--- app/models/user_statistics.rb
+++ app/models/user_statistics.rb
@@ -5,4 +5,13 @@ class UserStatistics < ApplicationRecord
   def total_answers
     self.right_answers + self.wrong_answers
   end
+
+  # Class method
+  def self.set_statistics(answer, current_user)
+    if !!current_user
+      user_statistics = UserStatistics.find_or_create_by!(user: current_user)
+      (answer.correct?) ? (user_statistics.right_answers += 1) : (user_statistics.wrong_answers += 1)
+      user_statistics.save
+    end
+  end
 end
